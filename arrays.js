bec4692761d7ca9816154c26a948e884cdeb60c5
Briana Baker <brianaclairbaker@gmail.com>
2017-11-07 01:50:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,18 @@
 var chocolateBars = ['chocolateBars', 'snickers', 'hundred grand', 'kit kat', 'skittles']
 
+function addElementToBeginningOfArray(array, element){
+ return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
