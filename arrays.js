0ff1469adfb80b3d47d9465deb3c9d3ac57b599e
Briana Baker <brianaclairbaker@gmail.com>
2017-11-07 02:05:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, element){
   return array[element];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
   return array
 }
